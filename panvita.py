2ffa87bfd8d97ef3358997ece44697b1c33dc84c
dlnrodrigues <68796413+dlnrodrigues@users.noreply.github.com>
2023-02-08 21:40:37 -0300

    Update panvita.py

--- panvita.py
+++ panvita.py
@@ -1196,6 +1196,9 @@ if ("-keep" not in sys.argv) and ("-k" not in sys.argv):
 
 print('''That's all, folks!
 Thank you for using this script.
+If you're going to use results from PanViTa, please cite us:
+PanViTa\thttps://doi.org/10.3389/fbinf.2023.1070406
+
 Do not forget to quote the databases used.\n''')
 if "-bacmet" in sys.argv:
 	print("BacMet\thttps://doi.org/10.1093/nar/gkt1252\t2014")
